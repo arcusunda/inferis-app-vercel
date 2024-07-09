4cd5c6da7b09230daf79f171cf33766a31230cbc
Arcus Unda <arcusunda@gmail.com>
2024-07-09 17:30:14 -0400

    coming soon

--- app/muertos/page.tsx
+++ app/muertos/page.tsx
@@ -69,7 +69,9 @@ export default function Home() {
           </li>
         ) : (
           <li className="relative pr-4 after:content-[''] after:block after:absolute after:top-0 after:right-0 after:h-full after:w-px after:bg-gray-400">
+            <span className="text-gray-500 cursor-not-allowed">
               Your Muertos
+             </span>
           </li>
         )}
         <li>
@@ -81,7 +83,6 @@ export default function Home() {
   
   return (
     <main className="flex min-h-screen flex-col items-left justify-between p-6">
-      {renderNavigation()}
       {!isConnected ? (
         <div className="flex flex-col items-center">
           {renderWelcomeContent()}
